207e1cecb092402211d3c153b5c017c9a270defe
daohieu0606 <damryo06@gmail.com>
2020-03-03 14:41:56 +0700

    update gitdemo.java

--- Downloads/gitdemo.java
+++ Downloads/gitdemo.java
@@ -2,5 +2,6 @@ class gitdemo {
     public static void main(String[] args) {
         System.out.println("hello world - git demo");
         //add new line at github
+        //co y dung do o local
     }
 }
